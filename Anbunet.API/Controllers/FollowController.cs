fb1d045fb4ca725d4ca5b0673c84e407366c46ea
VaSLib <75365404+VaSLib@users.noreply.github.com>
2024-05-26 18:46:58 +0600

    Created FollowController
    Created FollowController and followongs , followers endpoints

---
+++ Anbunet.API/Controllers/FollowController.cs
@@ -0,0 +1,33 @@
+﻿using Anbunet.Domain.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anbunet.API.Controllers;
+[Authorize]
+[ApiController]
+public class FollowController(ISender sender) : ControllerBase
+{
+    private readonly ISender _sender = sender;
+
+    [HttpGet("/followers/{userId}")]
+    public async Task<ActionResult<ValueResult<List<FollowResponse>>>> GetAll(long userId)
+    {
+        var command = new GetUserFollowersCommand(userId);
+
+        var response = await sender.Send(command);
+
+        return response.IsSuccess ? Ok(response) : BadRequest(response.Error);
+    }
+
+    [HttpGet("/followings/{userId}")]
+    public async Task<ActionResult<ValueResult<List<FollowResponse>>>> GetAll(long userId)
+    {
+        var command = new GetUserFollowingsCommand(userId);
+
+        var response = await sender.Send(command);
+
+        return response.IsSuccess ? Ok(response) : BadRequest(response.Error);
+    }
+
+}
