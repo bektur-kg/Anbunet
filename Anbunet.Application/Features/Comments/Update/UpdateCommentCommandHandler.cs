f9050dab24541df83c20f4ff304fe9b7176d7d76
CastBear <73561454+CastBear345@users.noreply.github.com>
2024-06-07 19:53:25 +0600

    fix update comment

--- Anbunet.Application/Features/Comments/Update/UpdateCommentCommandHandler.cs
+++ Anbunet.Application/Features/Comments/Update/UpdateCommentCommandHandler.cs
@@ -4,21 +4,28 @@ using Anbunet.Application.Services;
 using Anbunet.Domain.Abstractions;
 using Anbunet.Domain.Modules.Comments;
 using Anbunet.Domain.Modules.Posts;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace Anbunet.Application.Features.Comments.Update;
 
 public class UpdateCommentCommandHandler
     (
         ICommentRepository commentRepository,
+        IHttpContextAccessor httpContextAccessor,
         IUnitOfWork unitOfWork
         //IMapper mapper
     )
     : ICommandHandler<UpdateCommentCommand, Result>
 {
+    private readonly HttpContext _httpContext = httpContextAccessor.HttpContext!;
+    
     public async Task<Result> Handle(UpdateCommentCommand request, CancellationToken cancellationToken)
     {
+        var userId = long.Parse(_httpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
         var comment = await commentRepository.GetByIdWithInclude(request.Data.CommentId);
-        if (comment is null) return Result.Failure(CommentErrors.CommentNotFound);
+        if (comment is null || comment.UserId != userId) return Result.Failure(CommentErrors.CommentNotFound);
 
         comment.Text = request.Data.Text;
 
